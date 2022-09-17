c9de2838a9d12ccdfa55525be1c463d7f6cae19f
CalvienDanny <calviendannynariyana@gmail.com>
2022-09-17 00:37:58 -0400

    sisop

---
+++ Tugas1.py
@@ -0,0 +1,8 @@
+print ("-"*20)
+print("tugas 1 sisop kelas a")
+print("-"*20)
+print("nama:calvien danny nariyana")
+print("npm:21083010040")
+print("-"*20)
+print("halo dunia")
+print("ini adalah skrip bash pertamaku di linux")
